e92fc8baaf1f39ae2b43b7f2eda303f7124a8665
relaxed Ball <97145999+martinigv@users.noreply.github.com>
2023-09-27 00:58:42 +0900

    Time: 313 ms (7.72%), Space: 95.8 MB (8.8%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> transBinary(vector<vector<int>>& prev,int n,int max) {
+        // 0 1 01 11 001
+        if (n > max)
+            return prev;
+        int end = prev[n-1].size()-1;
+        vector<int> curr;
+        curr = prev[n-1];
+        curr[0]++;
+        for (int i = 0; i <= end; i++) {
+            if (curr[i] == 2) {
+                if (i == end) {
+                    curr[i] = 0;
+                    curr.push_back(1);
+                    continue;
+                }
+                curr[i] = 0;
+                curr[i+1]++;
+            }
+        }
+        for (auto v : curr) {
+            cout << v;
+        }
+        cout << endl;
+        prev.push_back(curr);
+        return transBinary(prev, n+1, max);
+    }
+    vector<int> countOne(vector<vector<int>> v) {
+        vector<int> res(v.size());
+        for (int i = 0; i < v.size(); i++) {
+            int count = 0;
+            for (int j = 0; j < v[i].size(); j++) {
+                if (v[i][j] == 1)
+                    count++;
+            }
+            res[i] = count;
+        }
+        return res;
+    }
+    vector<int> countBits(int n) {
+        vector<vector<int>> v;
+        vector<vector<int>> first = {{0}};
+        vector<int> ans;
+        v = transBinary(first, 1, n);
+        ans = countOne(v);
+        return ans;
+    }
+};
