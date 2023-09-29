0043406fc8edb03106deeda9e48ef4036be0dbac
relaxed Ball <97145999+martinigv@users.noreply.github.com>
2023-09-29 22:47:20 +0900

    Time: 16 ms (28.86%), Space: 20.6 MB (11.54%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int i = 0;
+        /* int i = 0;
         int fsize = flowerbed.size();
 
         while (i < fsize) {
             if (flowerbed[i] == 1) {
                 
-                if (i > 0) flowerbed[i-1] = -1;
-                if (i < fsize - 1) flowerbed[i+1] = -1;
+                if (i > 0) 
+                    flowerbed[i-1] = -1;
+                if (i < fsize - 1) 
+                    flowerbed[i+1] = -1;
             }
-            i++;
+            i += 2;
         }
 
         i = 0;
@@ -18,14 +20,32 @@ public:
             if (flowerbed[i] == 0) {
                 n--;
                 
-                if (i > 0) flowerbed[i-1] = -1;
-                if (i < fsize - 1) flowerbed[i+1] = -1;
+                if (i > 0) 
+                    flowerbed[i-1] = -1;
+                if (i < fsize - 1) 
+                    flowerbed[i+1] = -1;
                 i += 2; 
-            } else {
+            } 
+            else {
                 i++;
             }
         }
 
         return (n <= 0);
+        */
+        int count = 0;
+        for (int i = 0; i < flowerbed.size() && count < n; i++) {
+            if (flowerbed[i] == 0) {
+                // Get next and prev flower bed slot values. If i lies at the ends, the next and prev are considered as 0.
+                int next = (i == flowerbed.size() - 1) ? 0 : flowerbed[i + 1];
+                int prev = (i == 0) ? 0 : flowerbed[i - 1];
+                if (next == 0 && prev == 0) {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+            }
+        }
+
+        return count == n;
     }
-};
+}; 
