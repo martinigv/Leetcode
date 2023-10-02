ed5c3d38ac665893eab1b35a63073814e4c8707e
relaxed Ball <97145999+martinigv@users.noreply.github.com>
2023-10-02 20:20:42 +0900

    Time: 137 ms (20.97%), Space: 68.1 MB (24.52%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        /*int count = 0;
-        if (nums.size() < 2)
-            return count;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] < 0)
-                continue; 
-
-            int n = k - nums[i];
-            for (int j = nums.size()-1; j > i; j--) {
-                if (nums[j] < 0) 
-                    continue;
-
-                if (nums[j] == n) {
-                    count++;
-                    nums[i] = -1;
-                    nums[j] = -1; 
-                    break;  
-                }
-            }
-        }
-        return count;
-        */
         int ans = 0;
         unordered_map<int,int> mp;
 
