c4ae3c2b786604db41a2cc90e91c68050f4908ca
relaxed Ball <97145999+martinigv@users.noreply.github.com>
2023-10-06 15:43:53 +0900

    Time: 144 ms (15.91%), Space: 73.3 MB (7.18%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
         int ans = 0;
@@ -23,4 +23,21 @@ public:
         }
         return ans;
     }
+};*/
+class Solution {
+public:
+	int maxOperations(vector<int>& nums, int k) {
+		unordered_map<int, int> m;
+		int ans = 0;
+		for(int i = 0; i < nums.size(); i++){
+			if(m[k - nums[i]] > 0){
+			   m[k - nums[i]]--;
+				ans++;
+			}
+			else{
+				m[nums[i]]++;
+			}
+		}
+		return ans;
+	}
 };
