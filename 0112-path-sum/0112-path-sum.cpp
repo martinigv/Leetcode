e7054f7cf3026d5ac93f87b2b79fc238e135888d
relaxed Ball <97145999+martinigv@users.noreply.github.com>
2023-10-28 22:55:31 +0900

    Time: 13 ms (8.27%), Space: 21.6 MB (26.36%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int target;
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        target = targetSum;
+        return dfs(root, 0);
+    }
+    bool dfs(TreeNode* node, int curr) {
+        if (node == nullptr) {
+            return false;
+        }
+        if (node->left == nullptr && node->right == nullptr) {
+            return (curr + node->val) == target;
+        }
+        curr += node->val;
+        bool left = dfs(node->left, curr);
+        bool right = dfs(node->right, curr);
+        return left || right;
+    }
+};
